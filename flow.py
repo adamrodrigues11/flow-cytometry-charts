b156fc3453d3779da4e07d1610e53db37802b17d
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-10-17 18:15:33 -0700

    updated flow module to include validation of trials

--- flow.py
+++ flow.py
@@ -1,38 +1,44 @@
 import csv
+import math
+import matplotlib as plt
 
 
-# could abstract flow samples as a class to fully abstract the data structure
-
-class FlowFields:
-    def __init__(self, fields):
-        # check to make sure that the fields all have pos/neg for each marker and all boolean gating combinations
-        self.fields = fields
-
-    def get_length(self):
-        return len(self.fields)
-
-    def __iter__(self):
-        return iter(self.fields)
-
-    def __str__(self):
-        return str(self.fields)
+def is_valid_field_list(fields, num_markers):
+    # quick check to make sure that the fields all have pos/neg for each marker and all boolean gating combinations
+    return len(fields) == num_markers * 2 + 2 ** num_markers
 
 
-class FlowTrial:
+def is_valid_trial(trial, fields):
+    # check to make sure all the pos/neg pairs and bool gate proportions add to 100%
+    pairs = zip(fields.pos_markers, fields.neg_markers)
+    return all([math.isclose(trial[pos] + trial[neg], 100.0, rel_tol=1e-2) for pos, neg in pairs]) and (
+        math.isclose(sum([trial[field] for field in fields.bool_gates]), 100.0, rel_tol=1e-2)
+    )
+
+
+class Fields(list):
+    def __init__(self, fields, num_markers):
+        assert is_valid_field_list(fields, num_markers)
+        super().__init__(fields)
+        self.pos_markers = self[:num_markers]
+        self.neg_markers = self[num_markers: num_markers * 2]
+        self.bool_gates = self[num_markers * 2:]
+
+
+class Trial(dict):
     def __init__(self, fields, values):
         # fields is a FlowFields obj (conforms to field constraints)
-        assert fields.get_length() == len(values)  # ensure matches number of fields
-        # also check proportions add properly here or discard/raise
-        self.trial = dict(zip(fields, map(lambda x: float(x), values)))
-
-    def __str__(self):
-        return str(self.trial)
+        assert len(fields) == len(values)  # ensure a value for every field
+        trial = dict(zip(fields, map(lambda x: float(x), values)))
+        if not is_valid_trial(trial, fields):
+            raise Exception(f"Rejected invalid trial: {trial}")
+        super().__init__(trial)
 
     def aggregate_gated_markers_data(self):
         pass
 
 
-class FlowData:
+class DataSet:
     def __init__(self, fields, samples=None):
         # samples is a dictionary mapping sample ids to a collection of trials
         # fields are a list confirmed to adhere to field constraints
@@ -56,7 +62,7 @@ class FlowData:
         return self.samples.get(sample_id)
 
     def add_trial(self, sample_id, trial):
-        assert isinstance(trial, FlowTrial)
+        assert isinstance(trial, Trial)
         trials = self.get_trials(sample_id)
         if trials is None:
             self.add_sample(sample_id, [trial])
@@ -64,27 +70,30 @@ class FlowData:
             trials.append(trial)
 
     @staticmethod
-    def from_flojo_csv(file_name):
+    def from_flojo_csv(file_name, num_markers):
         with open(file_name) as csvfile:
             csv_reader = csv.reader(csvfile)
             raw_fields = csv_reader.__next__()[1:-1]  # ignore first and last column
-            fields = FlowFields(list(map(lambda x: x.split('/').pop().split('|')[0].strip(), raw_fields)))
-            flow_data = FlowData(fields)
+            fields = Fields(list(map(lambda x: x.split('/').pop().split('|')[0].strip(), raw_fields)), num_markers)
+            flow_data = DataSet(fields)
             for row in csv_reader:
                 sample_id = row.pop()  # remove last column from row, and name it sample_id
                 # guard that sample_id is not empty
                 if not sample_id:
                     continue
-                trial = FlowTrial(flow_data.fields, row[1:])  # ignore first column of csv row
-                flow_data.add_trial(sample_id, trial)
+                try:
+                    trial = Trial(flow_data.fields, row[1:])  # ignore first column of csv row
+                    flow_data.add_trial(sample_id, trial)
+                except Exception as e:
+                    print(sample_id, e)
+
             return flow_data
 
-    def get_markers(self):
-        return self.fields
-
     def get_num_markers(self):
-        return len(self.fields)
+        return len(self.fields.pos_markers)
 
     def aggregate_gated_markers_data(self):
         pass
 
+    def generate_plots(self):
+        pass
--- main.py
+++ main.py
@@ -2,47 +2,21 @@ import flow
 import pandas as pd
 
 
-# note: need to find more elegant way to remove sd and mean aggregate from csv output
+# note: need to find more elegant way to remove sd and mean aggregate from csv output ??
 # TODOS:
-# check that marker + and - data adds to 100% for each marker
-# check that all combination data over all marker combinations adds to 100%
 # aggregate the combination data
 # generate plots
-
-
-# def get_individual_markers(fields):
-#     for i in range(10):  # can be a max number of individual markers determined by a reasonable limit
-#         if len(fields) - (i * 2) == 2 ** i:
-#             return fields[:i]
-#     raise Exception('Fields are missing')
-
-
-# def check_is_data_complete(samples, fields):
-#     """Check to make sure each trial has data for each field.
-#     """
-#     for trials in samples.values():
-#         for trial in trials:
-#             assert len(trial) == len(fields)
-
-
-# def check_pos_neg_complete(fields, samples):
-#     markers = get_individual_markers(fields)
-#     num_markers = len(markers)
-#     for sample_id in samples:
-#         for trial in samples[sample_id]:
-#             for i in range(num_markers):
-#                 pos_plus_neg = trial[i] + trial[i + num_markers]
-#                 if not 99.9 < pos_plus_neg < 100.1:
-#                     samples[sample_id].remove(trial)  # error maybe a result of aliasing????
+# move the hardcoded number of markers to a command line arg
 
 
 def main():
-    flow_data = flow.FlowData.from_flojo_csv('test.csv')
+    flow_data = flow.DataSet.from_flojo_csv('test.csv', 5)
     print(flow_data)
     for sample_id, trials in flow_data.samples.items():
         print(sample_id)
         for trial in trials:
             print(trial)
+
 
 if __name__ == '__main__':
     main()
